028f4f3497e99e8a937a1b64519c667e155029bc
Joshua Ekollo <joshua87000@gmail.com>
2024-11-24 19:03:34 +0100

    Delete page.tsx

--- app/collections/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { Collection, CollectionFolder } from '@/types';
-import { useAuthContext } from '@/contexts/AuthContext';
-
-
